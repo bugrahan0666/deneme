27f783837d44990ea3794aec06cb192c1ea8662c
Glitch (bos-altyapii) <none>
2020-08-16 19:29:48 +0000

    🌧🌲 Checkpoint ./bot.js:13690389/4247

--- bot.js
+++ bot.js
@@ -222,42 +222,22 @@ client.on("warn", e => {
 client.on("error", e => {
   //Discord Code Share
 
-  console.log(chalk.bgRed(e.replace(regToken, "that was redacted"))); //Discord Code Share
-}); //Discord Code Share
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
 
-//Discord Code Share
 
-client.login(ayarlar.token); //Discord Code Share
+client.login(ayarlar.token);
 
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
-
-//Discord Code Share
 
 //........MESAJ ISTATISTIK........//
 
 client.on("message", async message => {
   if (message.author.bot === false) {
-    await db.add(`puan_${message.guild.id}_${message.author.id}`, 1); //MESAJ BAŞINA VERİLECEK PUAN ÜYE  //Discord Code Share
+    await db.add(`puan_${message.guild.id}_${message.author.id}`, 1); 
 
-    await db.add(`puanc_${message.guild.id}_${message.channel.id}`, 1); //MESAJ BAŞINA VERİLECEK PUAN KANAL   //Discord Code Share
+    await db.add(`puanc_${message.guild.id}_${message.channel.id}`, 1); 
 
-    await db.add(`puanuc_${message.author.id}_${message.channel.id}`, 1); //EN COK MESAJ ATILAN KANAL UYE  //Discord Code Share
+    await db.add(`puanuc_${message.author.id}_${message.channel.id}`, 1);
   }
 });
 
@@ -265,150 +245,33 @@ client.on("message", async message => {
 
 client.on("voiceStateUpdate", async (oldMember, newMember) => {
   if (!oldMember.user.bot) {
-    let oldChannel = oldMember.voiceChannel; //Discord Code Share
+    let oldChannel = oldMember.voiceChannel; 
 
-    let newChannel = newMember.voiceChannel; //Discord Code Share
+    let newChannel = newMember.voiceChannel;
 
     if (oldChannel === undefined && newChannel !== undefined) {
       db.set(`girisses.${oldMember.user.id}.${oldMember.guild.id}`, Date.now());
     } else if (newChannel === undefined) {
       let ilksessüre = await db.fetch(
         `girisses.${oldMember.user.id}.${oldMember.guild.id}`
-      ); //Discord Code Share
+      ); 
 
       let time = Date.now() - ilksessüre;
       await db.add(
         "voicei_" + oldMember.guild.id + "_" + oldMember.user.id,
         time
-      ); //Discord Code Share
+      ); 
 
       await db.add(
         "voicec_" + oldMember.guild.id + "_" + oldMember.voiceChannelID,
         time
-      ); //Discord Code Share
+      ); 
 
       await db.add(
         "voiceuc_" + oldMember.user.id + "_" + oldMember.voiceChannelID,
 
-        time //Discord Code Share
-      ); //Discord Code Share
-    }
-  } //Discord Code Share
-}); //Discord Code Share
-
-
-
-//İNVİTE STATS
-const invites = {};
-
-const wait = require("util").promisify(setTimeout);
-
-client.on("ready", () => {
-  wait(1000);
-
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-
-client.on("guildMemberRemove", async member => {
-  let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
-  if (!kanal) return;
-  let veri = await db.fetch(`rol1_${member.guild.id}`);
-  let veri12 = await db.fetch(`roldavet1_${member.guild.id}`);
-  let veri21 = await db.fetch(`roldavet2_${member.guild.id}`);
-  let veri2 = await db.fetch(`rol2_${member.guild.id}`);
-  let d = await db.fetch(`bunudavet_${member.id}`);
-  const sa = client.users.get(d);
-  const sasad = member.guild.members.get(d);
-  let sayı2 = await db.fetch(`davet_${d}_${member.guild.id}`);
-  db.add(`davet_${d}_${member.guild.id}`, -1);
-
-  if (!d) {
-    const aa = new Discord.RichEmbed()
-      .setColor("BLACK")
-      .setDescription(
-        `\`\`${member.user.tag}\`\` **adlı şahıs aramızdan ayrıldı.\nŞahsı davet eden:** \`\`Bulunamadı!\`\``
-      )
-      .setFooter(client.user.username, client.user.avatarURL);
-    client.channels.get(kanal).send(aa);
-    return;
-  } else {
-    const aa = new Discord.RichEmbed()
-      .setColor("BLACK")
-      .setDescription(
-        `\`\`${member.user.tag}\`\` **adlı şahıs aramızdan ayrıldı.\nŞahsı davet eden:** \`\`${sa.tag}\`\``
-      )
-      .setFooter(client.user.username, client.user.avatarURL);
-    client.channels.get(kanal).send(aa);
-
-    if (!veri) return;
-
-    if (sasad.roles.has(veri)) {
-      if (sayı2 <= veri12) {
-        sasad.removeRole(veri);
-        return;
-      }
-    }
-    if (sasad.roles.has(veri2)) {
-      if (!veri2) return;
-      if (sayı2 <= veri21) {
-        sasad.removeRole(veri2);
-        return;
-      }
+        time
+      ); 
     }
   }
-});
-
-client.on("guildMemberAdd", async member => {
-  member.guild.fetchInvites().then(async guildInvites => {
-    let veri = await db.fetch(`rol1_${member.guild.id}`);
-    let veri12 = await db.fetch(`roldavet1_${member.guild.id}`);
-    let veri21 = await db.fetch(`roldavet2_${member.guild.id}`);
-    let veri2 = await db.fetch(`rol2_${member.guild.id}`);
-    let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
-    if (!kanal) return;
-    const ei = invites[member.guild.id];
-
-    invites[member.guild.id] = guildInvites;
-
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const sasad = member.guild.members.get(invite.inviter.id);
-    const davetçi = client.users.get(invite.inviter.id);
-
-    db.add(`davet_${invite.inviter.id}_${member.guild.id}`, +1);
-    db.set(`bunudavet_${member.id}`, invite.inviter.id);
-    let sayı = await db.fetch(`davet_${invite.inviter.id}_${member.guild.id}`);
-
-    let sayı2;
-    if (!sayı) {
-      sayı2 = 0;
-    } else {
-      sayı2 = await db.fetch(`davet_${invite.inviter.id}_${member.guild.id}`);
-    }
-
-    const aa = new Discord.RichEmbed()
-      .setColor("BLACK")
-      .setDescription(
-        `\`\`${member.user.tag}\`\` **adlı şahıs sunucuya katıldı.\nŞahsı davet eden:** \`\`${davetçi.tag}\`\`\n**Toplam \`\`${sayı2}\`\` daveti oldu!**`
-      )
-      .setFooter(client.user.username, client.user.avatarURL);
-    client.channels.get(kanal).send(aa);
-    if (!veri) return;
-
-    if (!sasad.roles.has(veri)) {
-      if (sayı2 => veri12) {
-        sasad.addRole(veri);
-        return;
-      }
-    } else {
-      if (!veri2) return;
-      if (sayı2 => veri21) {
-        sasad.addRole(veri2);
-        return;
-      }
-    }
-  });
-});
+}); 
