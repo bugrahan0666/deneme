384fba51a3725729b7ac56cf6520117efa161f0a
Glitch (bos-altyapii) <none>
2020-08-22 19:25:48 +0000

    🏅💏 Checkpoint ./komutlar/0-moderasyon-yardım.js:13690389/313 ./komutlar/0-sil.js:13690389/2094 ./komutlar/1-sil.js:13690389/3660 ./komutlar/0-sunucu-yardım.js:13690389/2051

---
+++ komutlar/0-sunucu-yardım.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+   if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+      .setColor("BLACK")
+      .setTimestamp() 
+      .addField(`Hatalı Kullanım` , `Bu Komutu Özel Mesajlarda Kullanamazsınız Lütfen Sunucu İçerisinde Herhangi Bir Kanalı Kullanınız`)
+    return message.author.sendEmbed(ozelmesajuyari);
+  }
+  if (message.channel.type !== 'dm') {
+    const yardım = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle(`Elyse Sunucu Kurulum Komutları`)
+    .setDescription('Bu Yardım Menüsünde Sadece Sunucu Kurulum Komutları Hakkında Bilgi Verilmiştir')
+    .addField("Sunucu Kurulum Paketi ve Kullanımı",`> :small_blue_diamond: **e!sunucu-kur** = İşlemleri Onayladıktan Sonra Hazır Bir Sunucu Kurar\n> **Kullanımı:** __**e!sunucu-kur**__\n> :small_orange_diamond: **!kurallar-kur** = Hazır Sunucu Kuralları Listesi Oluşturur\n> **Kullanımı:** __**!kurallar-kur**__\n> :small_blue_diamond: **!sunucu-bilgi** = Sunucu Hakkında Bir Bilgilendirme Mesajı Olu\n> **Kullanımı:** __**!mesaj-top**__`)
+    .setFooter(message.author.username, message.author.avatarURL);
+    message.channel.sendEmbed(yardım) }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["sunucu-yardım","sunucu-komutları","sunucu-işlem-yardım"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sunucu-işlem-yardım',
+  description: 'Stats Komutlarını Gösterir',
+  usage: 'sunucu-işlem-yardım'
+};
