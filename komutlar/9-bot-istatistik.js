1dde4a75314be170ca07002aeb3c1ad5323c7756
Glitch (bos-altyapii) <none>
2020-08-22 22:20:13 +0000

    👐👧 Checkpoint ./komutlar/9-bot-istatistik.js:13690389/1146

--- komutlar/9-bot-istatistik.js
+++ komutlar/9-bot-istatistik.js
@@ -5,16 +5,14 @@ require("moment-duration-format");
 exports.run = (client, msg) => {
   const duration = moment.duration(client.uptime).format(" D [Gün], H [Saat], m [Dakika], s [Saniye]");
   msg.channel.sendCode(new Discord.RichEmbed()
-                       .setTitle('Elyse İstatistik')
-                       .setDescriptiom(`
-• Bellek kullanımı :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Çalışma süresi   :: ${duration}
-• Kullanıcılar     :: ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
-• Sunucular        :: ${client.guilds.size.toLocaleString()}
-• Kanallar         :: ${client.channels.size.toLocaleString()}
-• Discord.JS sürüm :: v${Discord.version}
-• Ping             :: ${client.ping}`))
-  .addField("Genel İstatistikler",`> Toplam Kullanıcı: **${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}**`)
+.setTitle('Elyse İstatistik')
+  .addField("Genel İstatistikler",`> Toplam Kullanıcı: **${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}**\n> Toplam Sunucu: **${client.guilds.size.toLocaleString()}**\n
+> Toplam Sunucu: **${client.guilds.size.toLocaleString()}**
+> Toplam Kanal: **${client.channels.size.toLocaleString()}**`)
+  .addField("Sürüm Bilgi",`> Discord.JS Sürümü: **v${Discord.version}**
+> Bellek Kullanımı: **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
+  .addField("Aktiflik ve Ping",`> Çalışma Süresi: **${duration}**
+> Ping: **${client.ping}**`));
 };
 
 exports.conf = {
