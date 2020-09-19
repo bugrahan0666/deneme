e96adfd5ba22261cbaeb8c0ecc712afaacb25fda
Glitch (fair-shelled-amusement) <none>
2020-09-19 14:50:03 +0000

    🍱⭐️ Checkpoint ./bot.js:22359355/1586 ./komutlar/afk.js:22359355/2912

--- bot.js
+++ bot.js
@@ -505,30 +505,26 @@ client.on("message", message => {
 });
 
 
-client.on("message" , async msg => {
-  if(msg.content.startsWith(ayarlar.prefix+"afk")) return;
- 
-  let afk = msg.mentions.users.first()
- 
-  const kisi = db.fetch(`afkid_${msg.author.id}_${msg.guild.id}`)
- 
-  const isim = db.fetch(`afkAd_${msg.author.id}_${msg.guild.id}`)
- if(afk){
-   const sebep = db.fetch(`afkSebep_${afk.id}_${msg.guild.id}`)
-   const kisi3 = db.fetch(`afkid_${afk.id}_${msg.guild.id}`)
-   if(msg.content.includes(kisi3)){
- 
-       msg.reply(`Etiketlediğiniz Kişi Afk \n Sebep : ${sebep}`)
-   }
- }
-  if(msg.author.id === kisi){
- 
-       msg.reply(`Afk'lıktan Çıktınız`)
-  db.delete(`afkSebep_${msg.author.id}_${msg.guild.id}`)
-  db.delete(`afkid_${msg.author.id}_${msg.guild.id}`)
-  db.delete(`afkAd_${msg.author.id}_${msg.guild.id}`)
-   msg.member.setNickname(isim)
-   
- }
- 
+const ms = require("parse-ms");
+client.on("message", async message => {
+  
+  if(message.author.bot) return;
+  if(!message.guild) return;
+  if(message.content.includes(`${prefix}afk`)) return;
+  
+  if(await db.fetch(`afk_${message.author.id}`)) {
+    db.delete(`afk_${message.author.id}`);
+    db.delete(`afk_süre_${message.author.id}`);
+    message.reply("Başarıyla afk modundan çıktınız.");
+  }
+  
+  var USER = message.mentions.users.first();
+  if(!USER) return;
+  var REASON = await db.fetch(`afk_${USER.id}`);
+  
+  if(REASON) {
+    let süre = await db.fetch(`afk_süre_${USER.id}`);
+    let timeObj = ms(Date.now() - süre);
+    message.channel.send(`${USER.tag} kullanıcısı AFK\n AFK süresi: ${timeObj.hours}h ${timeObj.minutes}m ${timeObj.seconds}s\nSebep:\n **${REASON}**` )
+  }
 });
--- komutlar/afk.js
+++ komutlar/afk.js
@@ -1,61 +1,28 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-const ayarlar = require("../ayarlar.json");
-exports.run = async (client, message, args) => {
-  const kisi = db.fetch(`afkid_${message.author.id}_${message.guild.id}`);
-  if (kisi) return;
-  const sebep = args[0];
-  if (!args[0]) {
-    let kullanıcı = message.guild.members.get(message.author.id);
-    const b = kullanıcı.displayName;
- 
-    await db.set(
-      `afkSebep_${message.author.id}_${message.guild.id}`,
-      "Sebep Girilmemiş"
-    );
-    await db.set(
-      `afkid_${message.author.id}_${message.guild.id}`,
-      message.author.id
-    );
-    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, b);
- 
-    const a = await db.fetch(
-      `afkSebep_${message.author.id}_${message.guild.id}`
-    );
- 
-    message.channel.send(`Başarıyla Afk Oldunuz \n Sebep: ${a}`);
- 
-    message.member.setNickname(`[AFK] ` + b);
-  }
-  if (args[0]) {
-    let sebep = args.join(" ");
-    let kullanıcı = message.guild.members.get(message.author.id);
-    const b = kullanıcı.displayName;
-    await db.set(`afkSebep_${message.author.id}_${message.guild.id}`, sebep);
-    await db.set(
-      `afkid_${message.author.id}_${message.guild.id}`,
-      message.author.id
-    );
-    await db.set(`afkAd_${message.author.id}_${message.guild.id}`, b);
-    const a = await db.fetch(
-      `afkSebep_${message.author.id}_${message.guild.id}`
-    );
- 
-    message.channel.send(`Başarıyla Afk Oldunuz \n Sebep: ${a}`);
- 
-    message.member.setNickname(`[AFK] ` + b);
-  }
-};
- 
+const db = require("quick.db")
+
+exports.run = function(client, message, args) {
+
+  
+  var USER = message.author;
+  var REASON = args.slice(0).join("  ");
+  if(!REASON) return message.channel.send("AFK olmak için bir sebep belirtin.");
+  
+  
+  db.set(`afk_${USER.id}`, REASON);
+  db.set(`afk_süre_${USER.id}`, Date.now());
+  message.channel.send("Başarıyla afk moduna girdiniz. ")
+  
+};  
+
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
+  enabled: true, 
+  guildOnly: true, 
   aliases: [],
-  permLevel: 0
+  permLevel: 0 
 };
- 
+
 exports.help = {
-  name: "afk",
-  description: "Afk Olmanızı Sağlar.",
-  usage: "afk / afk <sebep>"
+  name: 'afk', 
+  description: 'Kullanıcııyı afk moduna sokar.',
+  usage: 'afk <sebep>'
 };
