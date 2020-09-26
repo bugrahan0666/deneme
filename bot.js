c4834576f216f9c2dc327d16383a687774ec9c00
Glitch (fair-shelled-amusement) <none>
2020-09-26 12:21:50 +0000

    🚜🏥 Checkpoint ./bot.js:22359355/194

--- bot.js
+++ bot.js
@@ -35,6 +35,8 @@ client.on("guildMemberAdd", (member) => {
 member.setNickname ( "π İSİM | YAŞ ")
 })
 
+client.channels.get("756257487279227051").send(new Discord.RichEmbed().addField(`Krallığımıza yeni savaşçı katıldı!`,`Hemen ilgilenelim! <@$756248067669360811>`))
+
 app.listen(process.env.PORT); 
 
 setInterval(() => {
