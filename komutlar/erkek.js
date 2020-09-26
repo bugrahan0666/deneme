9cd63ad5d50968abe99ed2c0a9cd8cfdff8bd48d
Glitch (fair-shelled-amusement) <none>
2020-09-26 16:59:14 +0000

    🔨🎪 Checkpoint ./komutlar/erkek.js:22359355/3489 ./komutlar/kiz.js:22359355/3484

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -1,48 +1,42 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const moment = require('moment');
 const db = require('quick.db');
-exports.run = (client, message, args) => {
-var toplam = db.fetch(`toplamKayit_${message.author.id}`)
-  const erkek = message.guild.roles.find(r => r.id === "756250050757263450");
-  const misafir = message.guild.roles.find(r => r.id === "756255623422673058"); 
-  const log = message.guild.channels.find(c => c.id === "756257487279227051"); 
-  const tag = "π";
-  if(!message.member.roles.array().filter(r => r.id === "756248067669360811")[0]) { 
-    return message.channel.send("**Bu İşlemi Gerçekleştirmek İçin Kayıt Sorumlusu Olman Gerekli!**");
-  } else {
-    let member = message.mentions.users.first() || client.users.get(args.join(' '))
-      if(!member) return message.channel.send("Bir kullanıcı girin.")
-    const c = message.guild.member(member)
-    const nick = args[1];
-    const yas = args[2];
-      if(!nick) return message.channel.send("Bir isim girin.")
-      if(!yas) return message.channel.send("Bir yaş girin.")
-    c.addRole(erkek)
-    c.removeRole(misafir)
-    c.setNickname(`${tag} ${nick} | ${yas}`)
-    db.add(`erkekKayit_${message.author.id}`, 1)
-    db.add(`toplamKayit_${message.author.id}`, 1)
-    const embed = new Discord.RichEmbed()
-    .setAuthor("Phentos Kayıt Sistemi | Erkek Kayıt Yapıldı")
-    .addField(`<a:phentoselmas:758830318987378688> Kaydı yapılan\n`, `${c.user.tag}`)
-    .addField(`<a:phentoselmas:758830318987378688> Kaydı yapan\n`, `${message.author.tag}`)
-    .addField(`<a:phentoselmas:758830318987378688> Yeni isim\n`, `${tag} ${nick} , ${yas}`)
-    .addField(`<a:phentoselmas:758830318987378688> Toplam Kayıt\n`, toplam || 0)
-    .setFooter("Phentos Kayıt Sistemi | Developed By Phentos")
-    .setColor("RANDOM")
-    log.send(embed)
+module.exports.run = async (client, message, args) => {
+
+  if (!message.member.roles.has('kayıtçı rol id') && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Bu komudu kullanmak için yetkin yeterli değil.').then(m => m.delete(5000));
+
+  let user = message.mentions.users.first() || message.guild.members.get(args[0]);
+  let member = message.guild.member(user)
+  if (!user) return message.channel.send('Bir üye belirtmelisin.')
+
+  let isim = args[1];
+  if(!isim) return message.channel.send('Bir isim belirtmelisin.')
+  let yaş = Number (args[2]);
+  if(!yaş) return message.channel.send('Bir yaş belirtmelisin.')
+  let sayi = db.get(`erkeks_${message.author.id}`)
+  
+  if (!member.roles.has('erkek rol id')) {
+    member.addRole('erkek rolü id')
+    member.addRole('ikinci erkek rol id')
+    member.removeRole('kayıtsız rol id')
+    member.setNickname(`✦ ${isim} | ${yaş}`)
+    db.add(`erkeks_${message.author.id}`, +1)
+    message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`${user} adlı üye **erkek** olarak sunucumuza kaydedilmiştir.`).setFooter(`${message.author.tag}, toplam ${sayi || '1'} erkek kaydın mevcut.`).setTimestamp().setColor("BLACK"))
+  }else{
+    message.reply('Bu kişi zaten sunucumuza **erkek** olarak kaydedilmiş.')
   }
+
 }
 
-
-
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ["e"],
-  permLevel: 0
-};
-exports.help = {
-  name: "e",
-  description: "e",
-  usage: "e"
-};
+    enabled: true,
+    guildOnly: true,
+    aliases: ['e'],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'erkek',
+    description: '',
+    usage: ''
+  };
