ec1cbcc053cfad402335a40ea0535f9145862f6c
Glitch (phentosmod) <none>
2020-09-27 18:55:35 +0000

    👧🚔 Checkpoint ./komutlar/kanal-koruma.js:22359355/1661 ./komutlar/rol-koruma.js:22359355/2164 ./komutlar/temizle.js:22359355/768 ./bot.js:22359355/502

--- bot.js
+++ bot.js
@@ -373,3 +373,12 @@ message.send(taki)
  
          });
 
+
+// REKLAM \\
+client.on("message", async message => {
+    if (message.member.roles.find("id", "756241739093770302")) return;
+    let links = message.content.match(/(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi);
+    if (!links) return;
+    if (message.deletable) message.delete();
+    message.channel.send(`Hey ${message.author}, sunucuda link paylaşamazsın!`)
+})
