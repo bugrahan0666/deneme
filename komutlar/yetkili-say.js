3c4f9e91419e7f77ede2b2b14ca71b8a7eb5c01e
Glitch (phentosmod) <none>
2020-09-27 07:32:18 +0000

    🚤🐈 Checkpoint ./komutlar/yetkili-say.js:22359355/1755

--- komutlar/yetkili-say.js
+++ komutlar/yetkili-say.js
@@ -1,22 +1,24 @@
-const Discord = require("discord.js")
+const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args) => {
-    let sunucu = "756241474148106360";
-    let yetkili = message.guild.roles.get("756241739093770302").members;
-  const embed = new Discord.RichEmbed()
-    .addField("Sunucuda Bulunan Yetkili Sayısı ", `${yetkili.size}`)
-    .setFooter(client.user.username, message.guild.iconURL);
-  message.channel.sendEmbed(embed)
+exports.run = async (client, message, args) => {
+  let enAltYetkiliRolu = message.guild.roles.get("759668564876722207"); // en alttaki rol id
+  let yetkililer = message.guild.members.filter(uye => uye.highestRole.position >= enAltYetkiliRolu.position);
+  let embed = new Discord.RichEmbed().setTitle(message.guild.name + " Yetkili Sayımı").setTimestamp().setFooter(message.author.tag + " tarafından istendi!")
+  .setDescription(`Toplam Yetkili Sayısı: ${yetkililer.size}\nAktif Yetkili Sayısı: ${yetkililer.filter(uye => uye.presence.status !== "offline").size}\nSesli Kanalda Bulunanlar: ${yetkililer.filter(uye => uye.voiceChannel).size} | Bulunmayanlar: ${yetkililer.filter(uye => !uye.voiceChannel).size}`)
+  // Online �ye Say�s�: ${message.guild.members.filter(uye => uye.presence.status !== "offline").size}
+  message.channel.send(embed);
 };
+
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
+  enabled: true, 
+  guildOnly: true,
+  aliases: [], 
   permLevel: 0
 };
 
 exports.help = {
-  name: 'yetkili-say',
-  description: 'Yetkilileri Sayar',
-  usage: 'yetkili-say'
+  name: 'ysay', 
+  description: 'Yetkilileri sayar.', 
+  usage: 'yetkilisay',
+  kategori: 'yetkili'
 };
